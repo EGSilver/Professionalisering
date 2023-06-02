096ae2d4ec8e8543d546fcf54c0539280685767b
Dean Vervaeck <dean.vervaeck@outlook.com>
2023-06-02 02:21:27 +0200

    Workshop Fin

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        JavaDocExample javaDocExample = new JavaDocExample();
+        System.out.println(javaDocExample.berekenKwadraat(2));
+    }
+}
